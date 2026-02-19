2ccdb308cf5a9d0bcde5909cf806fea40dfe3323
Javi Guerra <javier.guerra@npower.org>
2026-02-19 11:02:50 -0800

    feat: created products dummy data to be able to be uploaded to mongodb

---
+++ src/data/products.js
@@ -0,0 +1,31 @@
+import easterEgg from '../assets/easter-eggs.jpg';
+import makeup from '../assets/makeup.jpg';
+import podium from '../assets/podium.jpg';
+import runningShoes from '../assets/running-shoes.jpg';
+
+export const products = [
+    {
+        name: "Easter Eggs",
+        description: "A collection of colorful Easter eggs, perfect for your Easter celebrations.",
+        price: 19.99,
+        image: easterEgg
+    },
+    {
+        name: "Makeup",
+        description: "A set of premium makeup products for all skin types.",
+        price: 29.99,
+        image: makeup
+    },
+    {
+        name: "Podium",
+        description: "An elegant podium for presentations and speeches.",
+        price: 49.99,
+        image: podium
+    },
+    {
+        name: "Running Shoes",
+        description: "Comfortable and durable running shoes for all terrains.",
+        price: 79.99,
+        image: runningShoes
+    }
+];

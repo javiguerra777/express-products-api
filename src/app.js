d487a24ed138efd50bbb58fd73e55d874ac89a92
Javi Guerra <javier.guerra@npower.org>
2026-02-19 10:30:38 -0800

    feat: added express cors dotenv as dependencies in package.json and nodemon as a dev dependency in package.json, added start script and dev script to be able to run in either production or development mode adjusted the src/app.js file to create an express api and created a root route that renders JSON of message that says Welcome to the API!

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
-console.log('Hello, World!');
+import dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Welcome to the API!' });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
